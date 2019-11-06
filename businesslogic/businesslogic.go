8e3184357a966bc5beaf3d9c5b4b13704279a484
Ken Lomax <ken.lomax@sap.com>
2019-11-06 10:21:44 +0100

    First commit

--- businesslogic/businesslogic.go
+++ businesslogic/businesslogic.go
@@ -1,7 +1,9 @@
 package businesslogic
 
+import "time"
+
 // F is a function that returns "Hello World"
 func F() string {
-	//time.Sleep(time.Millisecond * 50)
+	time.Sleep(time.Millisecond * 50)
 	return "Hello World"
 }
